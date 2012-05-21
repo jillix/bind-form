ee6dde4270dc40224efefc3b9a28d9a7f3188476
Adrian <adi@ottiker.com>
2012-05-21 17:33:18 +0200

    created main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+define(["./bind"], function(Bind){
+    
+    var Form = {
+        
+    };
+    
+    return function(config) {
+        
+        var form = N.clone(Form);
+        
+        return form;
+    }; 
+});
