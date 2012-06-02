2ac573c368c173a628ab923619c7441537afb60e
Adrian <adi@ottiker.com>
2012-06-02 14:04:33 +0200

    fill form via custom event

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-define(["adioo/bind/bind"], function(Bind){
+define(["adioo/bind/bind"], function(Bind) {
     
     var Form = {
         
+        fill: function(data) {
+            
+        },
+        
+        reset: function() {
+        
+        }
     };
     
-    return function(config) {
+    function init(config) {
         
-        var form = N.clone(Form);
+        var form = N.clone(Form, this);
+        
+        if (config.fillForm) {
+            
+            form.obs.l(config.fillForm, function(data) {
+                
+                form.fill(data);
+            });
+        }
+        
+        if (config.reset) {
+            
+            form.resetElm = form.dom.querySelector(config.reset);
+        }
         
         return form;
-    }; 
+    };
+    
+    return init;
 });
