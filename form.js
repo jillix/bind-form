280bc240f31b00894adf8f3cb97af74f0293c95c
Adrian Ottiker <adrian@ottiker.com>
2013-08-12 10:18:19 +0200

    removed dev code

--- form.js
+++ form.js
@@ -3,69 +3,6 @@ var Events = require('github/jillix/events');
 var data = require('./data');
 var ui = require('./ui');
 
-// TODO only for dev
-var devConfig = {
-    crud: 'crud', // crud miid
-    setTemplate: [''], // listen to setTemplate event from this miids
-    ui: {
-        target: '.form-container',
-        controls: {
-            save: 'button[name=save]',
-            remove: 'button[name=remove]',
-            cancel: 'button[name=cancel]'
-        },
-        progress: {}
-    }
-};
-var devTemplate = {
-    id: '52025bb30191894f19e2e82f',
-    name: 'Articles',
-    html: '/html/crud/frmTmplArticle.html',
-    schema: {
-        name: {
-            type: 'string', required: true,
-            label: 'Produkt', order: 5,
-            selectors: {
-                label: '.labelName',
-                value: 'input[name=name]'
-                // attr: 'attrName'
-            }
-        },
-        nr: {
-            type: 'string', required: true,
-            label: 'Art.-Nr', order: 10,
-            selectors: {
-                label: '.labelArticleNr',
-                value: '.articleNr'
-            }
-        },
-        price: {
-            type: 'number', required: true,
-            label: 'Bruttopreis', order: 20,
-            selectors: {
-                label: '.labelPrice',
-                value: 'input[name=price]'
-            }
-        },
-        discount: {
-            type: 'number',
-            label: 'Aktionspreis', order: 15,
-            selectors: {
-                label: '.labelDiscount',
-                value: 'input[name=discount]'
-            }
-        }
-    }
-};
-var devData = {
-    //"_id" : "5207b5b9986868041a000001",
-    "_tp" : "52025bb30191894f19e2e82f",
-    "nr" : "VP1",
-    "name" : "Behälter",
-    "price" : 10.1,
-    "discount" : 10.1
-};
-
 function setTemplate (template) {
     var self = this;
     
@@ -109,12 +46,6 @@ function init (config) {
         Events.call(self, config);
         
         self.emit('ready');
-        
-        // TODO only for dev
-        self.once('formRendered', function () {
-            self.emit('setData', devData);
-        });
-        self.emit('setTemplate', devTemplate);
     });
 }
 
